75628e8d906f6f3b2776402cefccb09d0e1406cf
brendanfreeman <bfreeman0@gmail.com>
2023-07-08 15:48:47 +0100

    Improve navigation menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,10 @@
+import 'remixicon/fonts/remixicon.css'
+
 let dropdown_menu = document.getElementById("dropdown");
 let dropdown_button = document.getElementById("menu-button")
 
-function dropping_menu(){
-    if(dropdown_menu.style.display === "block"){
+function dropping_menu() {
+    if (dropdown_menu.style.display === "block") {
         dropdown_menu.style.display = "none";
         dropdown_button.style.backgroundColor = "#fff";
         console.log("Block dropdown");
@@ -11,5 +13,5 @@ function dropping_menu(){
         dropdown_button.style.backgroundColor = "#000"
         console.log("none dropdown");
     }
-    
+
 }
